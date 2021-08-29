bc88f19ac89599238fb8cf54e52c70bccfafd53b
Jonathan van Wersch <jonathanvwersch@gmail.com>
2021-08-29 21:46:45 +0100

    Add redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,36 @@ import * as React from "react"
 import { Layout, Seo, Hero, GetStarted, SignUp, Features } from "../components"
 import { theme } from "dekked-design-system"
 import { ThemeProvider } from "styled-components"
+import Cookies from "js-cookie"
 
-const IndexPage = () => (
-  <ThemeProvider theme={theme()}>
-    <Layout>
-      <Seo title="Dekked" />
-      <Hero />
-      <GetStarted />
-      <Features />
-      <SignUp />
-    </Layout>
-  </ThemeProvider>
-)
+const IndexPage = () => {
+  const getSessionCookie = () => {
+    const sessionCookie = Cookies.get("session")
+
+    if (sessionCookie === undefined) {
+      return undefined
+    } else {
+      return sessionCookie
+    }
+  }
+
+  React.useLayoutEffect(() => {
+    if (getSessionCookie()) {
+      window.location.replace("https://app.dekked.com")
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme()}>
+      <Layout>
+        <Seo title="Dekked" />
+        <Hero />
+        <GetStarted />
+        <Features />
+        <SignUp />
+      </Layout>
+    </ThemeProvider>
+  )
+}
 
 export default IndexPage
